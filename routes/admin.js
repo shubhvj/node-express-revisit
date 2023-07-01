5e9a9b1f39561a784620e828e79e6efe55e4e98a
shubh <shubh.johri@torinit.ca>
2023-07-02 00:16:54 +0530

    added template layout

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,10 @@ const router = Express.Router();
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.render("add-product", { pageTitle: "Add Product" });
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
 router.post("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ const router = Express.Router();
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", { products: products });
+  res.render("shop", { products: products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
