d8bfb6a834efd2467a1b6573e27107309d967e8a
shubh <shubh.johri@torinit.ca>
2023-07-02 17:02:55 +0530

    added file storage

--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,18 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../helpers/path");
+const p = path.join(rootDir, "data", "products.json");
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, data) => {
+    if (err) {
+      return cb([]);
+    }
+    if (!err && JSON.parse(data).length > 0) {
+      return cb(JSON.parse(data));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title, description) {
@@ -6,9 +20,14 @@ module.exports = class Product {
     this.description = description;
   }
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
