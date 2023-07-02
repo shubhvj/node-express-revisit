6cade0948892a4da1e3900d3ef379fa28a150588
shubh <shubh.johri@torinit.ca>
2023-07-02 19:55:14 +0530

    added orders route and controller + adjustments

--- routes/shop.js
+++ routes/shop.js
@@ -6,6 +6,7 @@ const {
   getIndex,
   getCart,
   getCheckout,
+  getOrders
 } = require("../controllers/shop.controller");
 
 router.get("/", getIndex);
@@ -13,6 +14,8 @@ router.get("/", getIndex);
 router.get("/products", getProducts);
 
 router.get("/cart", getCart);
+
+router.get("/orders", getOrders);
 
 router.get("/checkout", getCheckout);
 
